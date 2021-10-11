d3e63cb9b6b1fc7abe098e4c67ce20fd08ecadbe
ajram12155 <91112133+ajram12155@users.noreply.github.com>
2021-10-11 20:16:58 +0530

    Create deployment-api.sh

---
+++ deployment-api.sh
@@ -0,0 +1,8 @@
+git clone git@bitbucket.org:Zencode_Projects/digitalxchange-api.git -b uat
+  
+cd digitalxchange-api
+# Building Artifact with Maven
+mvn clean install -DskipTests
+
+ #Coping JAR to the Server
+scp -r target/digitalexchange-*.war /opt/tomcat/webapps/de.war
